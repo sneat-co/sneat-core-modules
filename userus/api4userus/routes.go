7bbf463ccce9f8ff7aa7d70f82b3cb6aa40efd9d
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-12-20 18:47:15 +0000

    fix: login with Telegram draft

--- userus/api4userus/routes.go
+++ userus/api4userus/routes.go
@@ -9,5 +9,6 @@ import (
 func RegisterHttpRoutes(handle modules.HTTPHandleFunc) {
 	handle(http.MethodPost, "/v0/users/init_user_record", httpInitUserRecord)
 	handle(http.MethodPost, "/v0/users/set_user_country", httpSetUserCountry)
+	handle(http.MethodPost, "/v0/users/login_with_telegram", httpLoginWithTelegram)
 	//handle(http.MethodPost, "/v0/users/create_user", httpPostCreateUser)
 }
