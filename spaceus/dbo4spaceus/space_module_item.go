dd5c598f85c990af0f687cf7c0eeb330f7ce206b
Alexander Trakhimenok <alex@trakhimenok.com>
2025-03-22 08:02:38 +0000

    fix: NewSpaceModuleItemIncompleteKey

--- spaceus/dbo4spaceus/space_module_item.go
+++ spaceus/dbo4spaceus/space_module_item.go
@@ -4,11 +4,19 @@ import (
 	"github.com/dal-go/dalgo/dal"
 	"github.com/sneat-co/sneat-core-modules/linkage/dbo4linkage"
 	"github.com/sneat-co/sneat-go-core/coretypes"
+	"reflect"
 )
 
 func NewSpaceModuleItemKey[K comparable](spaceID coretypes.SpaceID, moduleID coretypes.ModuleID, collection string, itemID K) *dal.Key {
 	spaceModuleKey := NewSpaceModuleKey(spaceID, moduleID)
 	return dal.NewKeyWithParentAndID(spaceModuleKey, collection, itemID)
+}
+
+func NewSpaceModuleItemIncompleteKey[K comparable](spaceID coretypes.SpaceID, moduleID coretypes.ModuleID, collection string) *dal.Key {
+	spaceModuleKey := NewSpaceModuleKey(spaceID, moduleID)
+	var zero K
+	idKind := reflect.TypeOf(zero).Kind()
+	return dal.NewIncompleteKey(collection, idKind, spaceModuleKey)
 }
 
 func NewSpaceModuleItemKeyFromItemRef(itemRef dbo4linkage.SpaceModuleItemRef) *dal.Key {
